eb69596d8c218b0bf0c07f97200b15491ee7d825
ahwalid <walid_ahmadyar@yahoo.com>
2022-10-20 21:57:59 +0430

    React list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import * as React  from 'react';
 const list = [
-{
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  ObjectID:0,
-},
-{
-  title: 'Redux',
-  url:  'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  ObjectID:1,
-}
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    ObjectID:0,
+  },{
+    title: 'Redux',
+    url:  'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    ObjectID:1,
+  }
 ];
 
 
